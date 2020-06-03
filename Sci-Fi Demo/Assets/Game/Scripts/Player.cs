131d331fe499883b965abe67bb8992290f7c9156
TaaasT <61253797+TaaasT@users.noreply.github.com>
2020-06-03 10:01:45 +0200

    Player Movement

---
+++ Sci-Fi Demo/Assets/Game/Scripts/Player.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private CharacterController _controller;
+    [SerializeField]
+    private float _speed = 3.5f;
+    private float _gravity = 9.81f;
+
+    void Start()
+    {
+        _controller = GetComponent<CharacterController>();
+    }
+
+    
+    void Update()
+    {
+        Vector3 direction = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        Vector3 velocity = direction * _speed;
+        velocity.y -= _gravity;
+        _controller.Move(velocity * Time.deltaTime);
+    }
+}
