73c84e013a7d16998a99b87841d7b4401f0a68d7
TaaasT <61253797+TaaasT@users.noreply.github.com>
2020-06-03 11:25:50 +0200

    1

--- Sci-Fi Demo/Assets/Game/Scripts/LookY.cs
+++ Sci-Fi Demo/Assets/Game/Scripts/LookY.cs
@@ -15,6 +15,7 @@ public class LookY : MonoBehaviour
     void Update()
     {
         float _mouseY = Input.GetAxis("Mouse Y");
+
         Vector3 newRotation = transform.localEulerAngles;
         newRotation.x -= _mouseY;
         transform.localEulerAngles = newRotation;
