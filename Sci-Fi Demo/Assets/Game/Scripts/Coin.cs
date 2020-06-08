921a8ac0991458cb9b8ad92b99e4802a565712a6
TaaasT <61253797+TaaasT@users.noreply.github.com>
2020-06-08 10:39:25 +0200

    Coin Pickup Setup

---
+++ Sci-Fi Demo/Assets/Game/Scripts/Coin.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+    AudioSource _audioSource;
+
+    private void Start()
+    {
+        _audioSource = GetComponent<AudioSource>();
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Player" && Input.GetKeyDown(KeyCode.E))
+        {
+
+            _audioSource.Play();
+        }
+
+    }
+
+}
